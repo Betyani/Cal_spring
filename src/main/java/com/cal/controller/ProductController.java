e37b7e5e5978b463d0869f80ad6ef362ef0a0333
betyani <dhwhdgns777@gmail.com>
2025-07-10 10:58:26 +0900

    commit

--- src/main/java/com/cal/controller/ProductController.java
+++ src/main/java/com/cal/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.cal.controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cal.dto.ProductDto;
