c9d4f3a1e459389032c868bae8e1840c3b3cc802
heyiyou <billboy454@gmail.com>
2025-07-10 10:35:58 +0900

    1차 조회 파트 밑 상품 분류해서 볼수 있게

--- src/main/java/com/cal/service/ProductServiceImpl.java
+++ src/main/java/com/cal/service/ProductServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cal.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.cal.dto.ListDto;
 import com.cal.dto.ProductDto;
 import com.cal.mapper.ProductMapper;
 
@@ -24,4 +27,32 @@ public class ProductServiceImpl implements ProductService {
 	public void productDelete(int id) {
 		mapper.productDelete(id);
 	}
+	@Override
+	public ProductDto getProductById(int id) {
+		return mapper.findById(id);
+	}
+
+	@Override
+	public List<ProductDto> getProductsByCriteria(ListDto criteria) {
+		return mapper.findWithCriteria(criteria);
+	}
+
+	@Override
+	public int getProductCount(ListDto criteria) {
+		return mapper.countWithCriteria(criteria);
+	}
+
+	@Override
+	public List<ProductDto> getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertProduct(ProductDto product) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
