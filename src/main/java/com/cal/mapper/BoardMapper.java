4b9c7bfca03501bcf50956b597e9d566340ab745
heyiyou <billboy454@gmail.com>
2025-07-18 09:50:48 +0900

    2차 게시판 수정 부분

--- src/main/java/com/cal/mapper/BoardMapper.java
+++ src/main/java/com/cal/mapper/BoardMapper.java
@@ -1,7 +1,13 @@
 package com.cal.mapper;
 
+import java.util.List;
+
 import com.cal.dto.BoardDto;
 
 public interface BoardMapper {
 	public void boardRegister(BoardDto dto);
+	int updateBoard(BoardDto dto);
+	BoardDto selectBoardById(int id);
+	List<BoardDto> selectAllBoards();
+	
 }
--- src/main/java/com/cal/service/BoardServiceImpl.java
+++ src/main/java/com/cal/service/BoardServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cal.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.cal.dto.BoardDto;
@@ -8,16 +10,35 @@ import com.cal.mapper.BoardMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
-
 @Log4j
 @AllArgsConstructor
 @Service
 public class BoardServiceImpl implements BoardService {
-	
+
 	private BoardMapper mapper;
-	
+
 	@Override
 	public void boardRegister(BoardDto dto) {
 		mapper.boardRegister(dto);
 	}
+
+	@Override
+	public List<BoardDto> getBoardList() {
+		return mapper.selectAllBoards();
+	}
+
+	@Override
+	public BoardDto getBoardById(int id) {
+		return mapper.selectBoardById(id);
+	}
+
+	@Override
+	  public void updateBoard(int id, BoardDto dto) {
+	    dto.setId(id);
+	    if (mapper.updateBoard(dto) == 0) {
+	      System.out.println("⚠️ 수정 실패: 게시글 없음");
+	    }
+	    
+	}
+	
 }
